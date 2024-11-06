4def0375401909440ecfbf9694f6eddc7912596a
dharwee <168101942+dharwee@users.noreply.github.com>
2024-11-06 14:10:17 +0530

    Add files via upload

---
+++ fsd/Day 5/script.js
@@ -0,0 +1,48 @@
+/*
+function display(){
+    alert("hello JS");
+}
+
+function display(){
+    console.log("hello JS",x);
+}
+let x=14;
+console.log("Hello JS",x);
+display();
+
+let x=12.23;
+let y="hello JS";
+let z=true;
+let obj={
+    'name':'Rahul',
+    id:12,
+}
+let ar=[12,2,3,4,5];
+let a;
+console.log(typeof a);
+function validate(){
+    let un=document.getElementById("un").value;
+    let pass=document.getElementById("pass").value;
+    if(un=="admin" && pass=="admin")
+        document.getElementById("res").innerHTML="Login Admin";
+    
+    else
+        document.getElementById("res").innerHTML="Invalid Credentials";
+    
+}
+
+let x="12";
+console.log(typeof x);
+
+
+let a=()=>{
+    console.log("hello JS");
+};
+a(12,13);
+*/
+let say=()=>console.log("hello");
+say();
+
+function display(){
+    let p1
+}
